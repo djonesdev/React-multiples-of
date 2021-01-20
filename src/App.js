63a92792fc96c287c6eac5fa9fecd652ce284b3c
Dom Jones <domajones@outlook.com>
2021-01-20 04:57:56 +0000

    UI complete, css needs refactoring

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import Card from "./Components/Card/Card";
 import { getMultiplesFromList } from "./Utils/utils";
 
 function App() {
-  const [selectedNumber, setSelectedNumber] = useState()
-  const [fullNumberList, setFullNumberList] = useState(Array.from(Array(143).keys()))
-  const [listOfMutiples, setListOfMultiples] = useState([])
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [fullNumberList, setFullNumberList] = useState(
+    Array.from(Array(144).keys())
+  );
+  const [listOfMutiples, setListOfMultiples] = useState([]);
 
   useEffect(() => {
-    setListOfMultiples(getMultiplesFromList(fullNumberList, selectedNumber))
-  }, [selectedNumber, fullNumberList])
+    if (selectedNumber) {
+      setListOfMultiples(getMultiplesFromList(fullNumberList, selectedNumber));
+    }
+  }, [selectedNumber, fullNumberList]);
 
-  console.log(listOfMutiples, 'List of multiples')
+  const isMultipleOf = (number) =>
+    listOfMutiples.includes(number)
+      ? "c-app__touchable-card is-multiple"
+      : "c-app__touchable-card";
   return (
     <div className="App">
-      {fullNumberList.map((number, index) => (
-        <Card classNames={'c-app__touchable-card'} onClick={() => setSelectedNumber(index + 1)}>
-          <p>{index + 1}</p>
+      <div className="c-app__intro-container">
+        <p>
+          Select a number below to see which numbers are multiples of your
+          selected number!
+        </p>
+        <div className="c-app__select-number-list">
+          {listOfMutiples.map((number) => (
+            <Card>
+
+            <p className="c-app__select-number-list--item">{number}</p>
+            </Card>
+          ))}
+        </div>
+      </div>
+      {fullNumberList.map((number) => (
+        <Card
+          classNames={isMultipleOf(number + 1)}
+          onClick={() => setSelectedNumber(number + 1)}
+        >
+          <p>{number + 1}</p>
         </Card>
       ))}
     </div>
